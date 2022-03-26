949c87b717ae4167657258996dfd7a8342d8136b
mike carroll <thirteenthfloorcomic@gmail.com>
2022-03-25 21:26:25 -0700

    map changes

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 38.0, lng: -100 },
     zoom: 4,
   });
 }
