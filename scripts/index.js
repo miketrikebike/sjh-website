3c74dee8d2d15598647ea312e9f9457eeedffb1b
mike carroll <thirteenthfloorcomic@gmail.com>
2022-03-25 21:49:22 -0700

    fixed longitude

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 47.658, lng: 117.426 },
+    center: { lat: 47.658, lng: -117.426 },
     zoom: 10,
   });
 }
