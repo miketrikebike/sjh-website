59d77557c5573501ea915f9c03456d77e9f55748
mike carroll <thirteenthfloorcomic@gmail.com>
2022-03-25 21:12:26 -0700

    map changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,8 @@
-// [START maps_add_map]
-// Initialize and add the map
+let map;
+
 function initMap() {
-  // [START maps_add_map_instantiate_map]
-  // The location of Uluru
-  const uluru = { lat: 38.0, lng: -100.0 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
     zoom: 4,
-    center: uluru,
   });
-  // [END maps_add_map_instantiate_map]
-  // [START maps_add_map_instantiate_marker]
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-  // [END maps_add_map_instantiate_marker]
 }
-// [END maps_add_map]
